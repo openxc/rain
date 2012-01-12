d2bb58e49f1c2093d36b14216c4c3a03e38bfaf2
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-01-12 13:02:38 -0500

    Periodically upload wiper status to wunderground.com.

--- src/main/java/com/ford/openxc/rain/RainMonitorActivity.java
+++ src/main/java/com/ford/openxc/rain/RainMonitorActivity.java
@@ -1,10 +1,20 @@
 package com.ford.openxc.rain;
 
+import java.io.IOException;
+
+import org.apache.http.client.methods.HttpGet;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -17,31 +27,108 @@ import com.openxc.measurements.NoValueException;
 import com.openxc.measurements.UnrecognizedMeasurementTypeException;
 import com.ford.openxc.rain.R;
 import com.openxc.measurements.WindshieldWiperStatus;
+import com.openxc.measurements.Latitude;
+import com.openxc.measurements.Longitude;
 import com.openxc.remote.RemoteVehicleServiceException;
 
 public class RainMonitorActivity extends Activity {
 
     private static String TAG = "RainMonitorActivity";
+    private final String WUNDERGROUND_URL =
+        "http://www.wunderground.com/weatherstation/VehicleWeatherUpdate.php";
 
     private VehicleService mVehicleService;
     private boolean mIsBound;
     private TextView mWiperStatusView;;
     private final Handler mHandler = new Handler();
 
-    WindshieldWiperStatus.Listener mWiperListener =
-            new WindshieldWiperStatus.Listener() {
-        public void receive(VehicleMeasurement measurement) {
-            final WindshieldWiperStatus wiperStatus =
-                (WindshieldWiperStatus) measurement;
-            if(!wiperStatus.isNone()) {
+    private Runnable mCheckWipersTask = new Runnable() {
+        public void run() {
+            final Latitude latitude;
+            final Longitude longitude;
+            final WindshieldWiperStatus wiperStatus;
+            try {
+                latitude = (Latitude) mVehicleService.get(Latitude.class);
+                longitude = (Longitude) mVehicleService.get(Longitude.class);
+                wiperStatus = (WindshieldWiperStatus) mVehicleService.get(
+                        WindshieldWiperStatus.class);
+            } catch(UnrecognizedMeasurementTypeException e) {
+                return;
+            }
+
+            if(!latitude.isNone() && !longitude.isNone() &&
+                    !wiperStatus.isNone()) {
+                final boolean wiperStatusValue;
+                try {
+                    wiperStatusValue =
+                        wiperStatus.getValue().booleanValue();
+                } catch(NoValueException e) {
+                    Log.w(TAG, "Expected wiper status to have a value " +
+                            "but it did not", e);
+                    return;
+                }
+
                 mHandler.post(new Runnable() {
                     public void run() {
-                        try {
-                            mWiperStatusView.setText("" +
-                                wiperStatus.getValue().booleanValue());
-                        } catch(NoValueException e) { }
+                        String wiperText;
+                        if(wiperStatusValue) {
+                            wiperText = "on";
+                        } else {
+                            wiperText = "off";
+                        }
+                        mWiperStatusView.setText(wiperText);
                     }
                 });
+                uploadWiperStatus(latitude, longitude, wiperStatus);
+                // Repeat every 5 minutes or 300,000ms
+                mHandler.postDelayed(this, 300000);
+            }
+        }
+
+        private void uploadWiperStatus(Latitude latitude, Longitude longitude,
+                WindshieldWiperStatus wiperStatus) {
+            double latitudeValue;
+            double longitudeValue;
+            boolean wiperStatusValue;
+            try {
+                latitudeValue = latitude.getValue().doubleValue();
+                longitudeValue = longitude.getValue().doubleValue();
+                wiperStatusValue = wiperStatus.getValue().booleanValue();
+            } catch(NoValueException e) {
+                Log.w(TAG, "Expected measurements to have a value, " +
+                        "but at least one didn't -- skipping upload", e);
+                return;
+            }
+
+            if(!wiperStatusValue) {
+                Log.d(TAG, "Wipers are off -- not uploading");
+                return;
+            }
+
+            StringBuilder uriBuilder = new StringBuilder(WUNDERGROUND_URL);
+            // TODO need to update API to accept boolean instead of speed
+            int wiperSpeed = 0;
+            if(wiperStatusValue) {
+                wiperSpeed = 1;
+            }
+            uriBuilder.append("?wiperspd=" + wiperSpeed);
+            uriBuilder.append("&lat=" + latitudeValue);
+            uriBuilder.append("&lon=" + longitudeValue);
+
+            final HttpClient client = new DefaultHttpClient();
+            HttpGet request = new HttpGet(uriBuilder.toString());
+            try {
+                HttpResponse response = client.execute(request);
+                final int statusCode = response.getStatusLine().getStatusCode();
+                if(statusCode != HttpStatus.SC_OK) {
+                    Log.w(TAG, "Error " + statusCode +
+                        " while uploading wiper status");
+                } else {
+                    Log.d(TAG, "Wiper status (" + wiperStatus + ") uploaded " +
+                        "successfully");
+                }
+            } catch(IOException e) {
+                Log.w(TAG, "Error while uploading wiper status", e);
             }
         }
     };
@@ -52,16 +139,9 @@ public class RainMonitorActivity extends Activity {
             Log.i(TAG, "Bound to VehicleService");
             mVehicleService = ((VehicleService.VehicleServiceBinder)service
                     ).getService();
-
-            try {
-                mVehicleService.addListener(WindshieldWiperStatus.class,
-                        mWiperListener);
-            } catch(RemoteVehicleServiceException e) {
-                Log.w(TAG, "Couldn't add listeners for measurements", e);
-            } catch(UnrecognizedMeasurementTypeException e) {
-                Log.w(TAG, "Couldn't add listeners for measurements", e);
-            }
             mIsBound = true;
+
+            mHandler.postDelayed(mCheckWipersTask, 100);
         }
 
         public void onServiceDisconnected(ComponentName className) {
