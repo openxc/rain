7f61d00409b6e7750c98f543de19f762fa4c8b34
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-02-23 17:17:21 -0500

    Avoid wildcard imports, just grab what's used.

--- src/main/java/com/ford/openxc/rain/RainMonitorActivity.java
+++ src/main/java/com/ford/openxc/rain/RainMonitorActivity.java
@@ -5,8 +5,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.*;
-import java.io.*;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.apache.http.client.methods.HttpGet;
 
